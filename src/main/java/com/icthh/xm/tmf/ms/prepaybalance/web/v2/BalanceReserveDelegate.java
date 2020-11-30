d7ed7b23f4ecf82c3e38dfc90a0f15ce9509cce7
yekalinichenko <pkev777@gmail.com>
2020-11-30 20:17:43 +0200

    Added balance reserve delegate implementation for api v2

---
+++ src/main/java/com/icthh/xm/tmf/ms/prepaybalance/web/v2/BalanceReserveDelegate.java
@@ -0,0 +1,28 @@
+package com.icthh.xm.tmf.ms.prepaybalance.web.v2;
+
+import com.icthh.xm.commons.lep.LogicExtensionPoint;
+import com.icthh.xm.commons.lep.spring.LepService;
+import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
+import com.icthh.xm.tmf.ms.prepaybalance.lep.keyresolver.ProfileKeyResolver;
+import com.icthh.xm.tmf.ms.prepaybalance.web.v2.api.BalanceReserveApiDelegate;
+import com.icthh.xm.tmf.ms.prepaybalance.web.v2.api.model.BalanceReserveBody;
+import com.icthh.xm.tmf.ms.prepaybalance.web.v2.api.model.BalanceReserveRequest;
+import io.micrometer.core.annotation.Timed;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+@Component
+@LepService(group = "service", name = "default")
+public class BalanceReserveDelegate implements BalanceReserveApiDelegate {
+
+    @Timed
+    @LogicExtensionPoint(value = "PrepayBalanceReserve", resolver = ProfileKeyResolver.class)
+    @PreAuthorize("hasPermission({'profile': @headerRequestExtractor.profile}, 'PREPAYBALANCE.BALANCE.RESERVE')")
+    @Override
+    @PrivilegeDescription("Privilege to create reserve balance operation")
+    public ResponseEntity<BalanceReserveBody> createReserveOperation(BalanceReserveRequest balanceReserveRequest) {
+        return ResponseEntity.ok().build();
+    }
+}
+
